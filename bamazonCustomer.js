eb24d6e0a0474854a08e9279e2450d402f238978
Lebeza Wendemsesha <BezMack@Lebezas-MBP.fios-router.home>
2017-08-23 22:22:25 -0400

    add img folder

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,80 +1,115 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var inq = require("inquirer");
+var db = require("./sqlConnect.js");
 var Table = require("cli-table");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "Bamazon"
-});
+welcome();
 
 
-connection.connect(function(err) {
- if (err) throw err;
- console.log("Connected!");
- customerOrder();
-});
+function welcome(){
 
-var table = new Table({
-    head: ['ID', 'Product','Price ($)', 'QTY'],
-});
+  console.log(
+`
+***********************************************************
+                  WELCOME TO bAMAZON!
+        Here is a list of our current Merchandise.
+***********************************************************
 
-// Array of all the IDs to be used for validation later.
-var idArr = [];
+`
+);
 
-// Query Database for the relevant information.
-connection.query('SELECT * FROM products', function (error, res, fields) {
-  if (error) throw error;
-  for (var i = 0; i < res.length; i++) {
-    var tRow = [];
-    tRow.push(res[i].item_id,res[i].product_name,res[i].price,res[i].stock_quantity);
-    table.push(tRow);
-    idArr.push(res[i].item_id);
-  }
-  // Push new rows to table.
-  console.log(table.toString());
-  customerOrder(idArr);
-});
+  // Create new Table Object.
+  var table = new Table({
+      head: ['ID', 'Product','Price ($)', 'QTY'],
+  });
 
 
- // function which prompts the user for what action they should take
-function customerOrder() {
-   inquirer.prompt([
-     {
-       name: "item-id",
-       type: "input",
-       message: "What productId would you like to buy?"
-     },
-     {
-       name:"stock_quantity",
-       type:"input",
-       message:"Enter the Quantity that you wish to purchase: ",
-       default: 1
-     }
-     ]).then(function(choice){
-         var pid = parseInt(choice.item_id);
-         var sq = parseInt(choice.stock_quantity);
+  var id = [];
 
-         // Check that both values are Numeric.
-         if(pid || sq === NaN){
-           console.log("\nPlease enter Numeric values for ID and QTY.\n")
-           customerOrder(array);
-         }else{
-           // Check if customer entered a non-existant ID number.
-           if(array.indexOf(pid) === -1){
-             console.log("\nIt seems like that product doesn't exist. Please enter a valid ID.\n")
+  // Query Database for the relevant information.
+  db.query('SELECT * FROM products', function (error, res, fields) {
+    if (error) throw error;
+    for (var i = 0; i < res.length; i++) {
+      var tRow = [];
+      tRow.push(res[i].item_id,res[i].product_name,res[i].price,res[i].stock_quantity);
+      table.push(tRow);
+      id.push(res[i].item_id);
+    }
+    // Push new rows to table.
+    console.log(table.toString());
+    customerOrder(id);
+  });
+
+}
+
+
+// Grab Customer Input
+function customerOrder(array){
+  inq.prompt([
+    {
+      name:"productID",
+      type:"input",
+      message:"Enter the ID number of the product you wish to purchase: ",
+    },
+    {
+      name:"productQTY",
+      type:"input",
+      message:"Enter the Quantity that you wish to purchase: ",
+      default: 1,
+    }
+    ]).then(function(choice){
+        var pid = parseInt(choice.productID);
+        var pq = parseInt(choice.productQTY);
+
+        // Check that both values are Numeric.
+        if(isNaN(pid) || isNaN(pq)){
+          console.log("\nPlease enter Numeric values for ID and QTY.\n")
+          customerOrder();
+        }else{
+          // Check if customer entered a non-existant ID number.
+          if(array.indexOf(pid) === -1){
+            console.log("\nIt seems like that product doesn't exist. Please enter a valid ID.\n")
             customerOrder(array);
-           }else{
-             //checkQty(pid, sq, array);
-             console.log("Congrats! we have what you want");
-           }
-       }
+          }else{
+            checkQty(pid, pq, array);
+          }
+        }
 
-   });
+    });
+}
+
+
+// Check Quantity
+function checkQty(id, qty, array){
+
+  db.query('SELECT * FROM products WHERE item_id=?',id, function (error, res, fields) {
+    if (error) throw error;
+
+    var leftOverQty = res[0].stock_quantity - qty;
+    var itemPrice = res[0].price;
+    var deptId = res[0].department_id;
+    var prodId = id;
+
+    if (leftOverQty < 0){
+      console.log("\nInsufficient Quantity!\n");
+      customerInput(array);
+    } else {
+      let totalPrice = itemPrice * qty;
+      fulfillOrder(prodId, leftOverQty, totalPrice);
+    }
+
+  });
+}
+
+// Fulfill Order
+function fulfillOrder(id, qty, price){
+  db.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?',[qty, id], function (error, res, fields) {
+    if (error) throw error;
+
+    console.log('<h2>Thank you for Shopping with us today! Your total is: $ </h2>' + [price.toFixed(2)]
+
+);
+
+
+  });
 }
